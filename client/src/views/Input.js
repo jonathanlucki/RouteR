48d1db78f9edf9be5ccab553fb62e5b44005beb5
Jonathan Lucki <lucki.jonathan@gmail.com>
2019-05-17 19:53:03 -0400

    Initial client react setup

---
+++ client/src/views/Input.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+class Input extends React.Component {
+
+    //component constructor
+    constructor(props) {
+        //set props
+        super(props);
+    }
+
+    //component render method
+    render() {
+        return (<Typography variant="h4" gutterBottom>test</Typography>);
+    }
+
+}
+
+export default Input;
